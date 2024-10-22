7455565a82808c887e777f3657cfbea0352f08cc
Nataliia Ishchenko <nataliia.ishchenko@desifer.se>
2024-10-22 11:23:26 +0200

    More exicises

--- restaurants.py
+++ restaurants.py
@@ -76,10 +76,76 @@ collection = db['restaurants']
 #     print(document)
 
 #10. Write a MongoDB query to find the restaurants which locate in latitude value less than -95.754168.
-results = collection.find({"address.coord": { "$lt": -95.754168 } }, {"name": 1, "address": 1, "_id": 0})
+# results = collection.find({"address.coord": { "$lt": -95.754168 } }, {"name": 1, "address": 1, "_id": 0})
+# for document in results:
+#     print(document)
+
+# 11. Write a MongoDB query to find the restaurants that do not prepare any cuisine of 'American' and their grade score more than 70 and latitude less than -65.754168.
+# results = collection.find(
+#     {"$and":[
+#     {"grades": {
+#          "$elemMatch": { "score": { "$gt": 70} }
+#      }}, 
+#      {"address.coord": { "$lt": -65.754168 } },
+#      {"cuisine" : {"$ne" :"American "}},
+#     ]}, {"name": 1, "address": 1, "cuisine":1, "_id": 0})
+# for document in results:
+#     print(document)
+
+#12. Write a MongoDB query to find the restaurants which do not prepare any cuisine of 'American' and achieved a score more than 70 and located in the longitude less than -65.754168.
+# Note : Do this query without using $and operator.
+# results = collection.find(
+#     {
+#     "grades": {
+#          "$elemMatch": { "score": { "$gt": 70} }
+#      }, 
+#      "address.coord": { "$lt": -65.754168 },
+#      "cuisine" : {"$ne" :"American "},
+#     }, {"name": 1, "address": 1, "cuisine":1, "_id": 0})
+# for document in results:
+#     print(document)
+
+#13. Write a MongoDB query to find the restaurants which do not prepare any cuisine of 'American' and achieved a grade point 'A' not belongs to the borough Brooklyn. The document must be displayed according to the cuisine in descending order.
+# results = collection.find(
+#     {
+#     "grades": {
+#          "$elemMatch": { "grade": { "$eq": "A"} }
+#      }, 
+#      "borough": { "$ne": "Brooklyn" },
+#      "cuisine" : {"$ne" :"American "},
+#     }, {"name": 1, "borough": 1, "cuisine": 1, "_id": 0}).sort({"cuisine": -1})
+# for document in results:
+#     print(document)
+
+# 14. Write a MongoDB query to find the restaurant Id, name, borough and cuisine for those restaurants which contain 'Wil' as first three letters for its name.
+# results = collection.find(
+#     { "name": { "$regex": "^Wil", "$options": "i" } },
+#     { "restaurant_id": 1, "name": 1, "borough": 1, "cuisine": 1, "_id": 0 }
+# )
+
+# 15. Write a MongoDB query to find the restaurant Id, name, borough and cuisine for those restaurants which contain 'ces' as last three letters for its name.
+# results = collection.find(
+#     { "name": { "$regex": "ces$", "$options": "i" } },
+#     { "restaurant_id": 1, "name": 1, "borough": 1, "cuisine": 1, "_id": 0 }
+# )
+
+#16. Write a MongoDB query to find the restaurant Id, name, borough and cuisine for those restaurants which contain 'Reg' as three letters somewhere in its name.
+# results = collection.find(
+#     { "name": { "$regex": "Reg", "$options": "i" } },
+#     { "restaurant_id": 1, "name": 1, "borough": 1, "cuisine": 1, "_id": 0 }
+# )
+
+# 17. Write a MongoDB query to find the restaurants which belong to the borough Bronx and prepared either American or Chinese dish.
+results = collection.find(
+    { "borough": { "$eq": "Bronx" },
+     "cuisine": {"$in": ["American ", "Chinese"]}},
+    { "name": 1, "borough": 1, "cuisine": 1, "_id": 0 }
+)
+
+#18. Write a  MongoDB query to find the restaurant Id, name, borough and cuisine for those restaurants which belong to the borough Staten Island or Queens or Bronxor Brooklyn.
+
 for document in results:
     print(document)
 
-    
 # Close the connection when done
 client.close()
